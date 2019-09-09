8f95b41d4704a40f493e87b63658f42326d12f57
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-09 10:24:21 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//Write a Java program that assigns your name to a variable and prints the variable to the console.
+public class Main {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        System.out.println(name);
+    }
+}
